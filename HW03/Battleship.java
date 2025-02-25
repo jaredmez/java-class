a6723447b7d5ad348640c51449ebc939abef32c0
Jared M <mezquitaj@gmail.com>
2025-02-25 15:53:50 -0600

    battle board initialized with proper inputs

--- HW03/Battleship.java
+++ HW03/Battleship.java
@@ -8,8 +8,8 @@ public class Battleship {
         char[][] playerOneBoard = new char[5][5];
         char[][] playerTwoBoard = new char[5][5];
         fillBoardWithDashes(playerOneBoard);
-        fillBoardWithUserCoordinates(3, playerOneBoard);
-//        printBattleShip(fillBoardWithDashes(playerOneBoard));
+        fillBoardWithUserCoordinates(1, playerOneBoard);
+        printBattleShip(playerOneBoard);
     }
 
     // Use this method to print game boards to the console.
@@ -39,19 +39,42 @@ public class Battleship {
         return board;
     }
 
-    private static void fillBoardWithUserCoordinates(int player, char[][] board) {
+    private static char[][] fillBoardWithUserCoordinates(int player, char[][] board) {
         Scanner sc = new Scanner(System.in);
         System.out.printf("PLAYER %d, ENTER YOUR SHIPS' COORDINATES.\n", player);
         int i = 1;
-        System.out.println(board[1][1]);
-//        do {
-//            System.out.printf("Enter ship %d location:\n", i);
-//            int coordOne = sc.nextInt();
-//            int coordinateTwo = sc.nextInt();
-//            System.out.println("you entered: " + coordOne + " and " + coordinateTwo);
-//            i++;
-//        }
-//        while (i < 6);
 
+        do {
+            System.out.printf("Enter ship %d location:\n", i);
+            int coordinateOne = sc.nextInt();
+            int coordinateTwo = sc.nextInt();
+            if(validInputCheck(coordinateOne) && validInputCheck(coordinateTwo) && !isDuplicateCheck(coordinateOne, coordinateTwo, board)){
+                board[coordinateOne][coordinateTwo] = '@';
+                i++;
+            } else if (!validInputCheck(coordinateOne) || !validInputCheck(coordinateTwo)) {
+                System.out.println("Invalid coordinates. Choose different coordinates.\n");
+            }
+            else {
+                System.out.println("You already fired on this spot. Choose different coordinates.");
+            }
+        }
+        while (i < 6);
+        return board;
+    }
+
+    private static boolean validInputCheck(int userInput) {
+        if(userInput < 0 || userInput >= 5) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private static boolean isDuplicateCheck(int one, int two, char[][] userBoard) {
+        boolean duplicate = false;
+        if(userBoard[one][two] == '@') {
+            duplicate = true;
+        }
+        return duplicate;
     }
 }
